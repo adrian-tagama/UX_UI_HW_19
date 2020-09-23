c2f7e506e91ba413cf1d5799a51cb0ab5eb1be88
Adrian <60756264+adrian-tagama@users.noreply.github.com>
2020-09-23 00:02:45 -0700

    Make hamburger menu

--- js/index.js
+++ js/index.js
@@ -9,3 +9,9 @@ $("#down-arrow").click(function() {
     }, 2000);
 });
 
+const menuButton = document.getElementsByClassName('menu-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+menuButton.addEventListener('click',() => {
+    navbarLinks.classList.toggle('active')
+})
